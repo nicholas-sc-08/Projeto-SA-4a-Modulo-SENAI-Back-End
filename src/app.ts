bbebed48cc1a1e97c8c2a09692ea1fc386eeca21
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-09-04 22:15:33 -0300

    feat: rota dos produtos feita

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import router_produto from "./Routes/router.produto";
 
 const app = express();
 
 app.use(express.json());
+app.use(`/produtos`, router_produto);
 
 export default app;
